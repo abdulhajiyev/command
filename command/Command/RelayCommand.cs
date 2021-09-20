5d0368aad6e147ea871118fe341a5d8e1720cf82
Abdul Hajiyev <abdulhajiyev@gmail.com>
2021-09-20 20:38:43 +0400

    1

---
+++ command/Command/RelayCommand.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace command.Command
+{
+    public class RelayCommand : ICommand
+    {
+        private Action<object> _execute { get; set; }
+        public Predicate<object> _canExecute { get; set; }
+
+        public RelayCommand(Action<object> execute, Predicate<object> canExecute = null)
+        {
+            _execute = execute ?? throw new NullReferenceException("Execute is null");
+            _canExecute = canExecute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute?.Invoke(parameter) ?? true;
+        }
+
+        public void Execute(object parameter)
+        {
+            _execute.Invoke(parameter);
+        }
+
+        public event EventHandler CanExecuteChanged;
+    }
+}
