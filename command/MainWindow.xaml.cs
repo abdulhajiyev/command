1ff1eecf17abbd61a22120c617c142e4786d708f
Abdul Hajiyev <abdulhajiyev@gmail.com>
2021-09-20 19:44:23 +0400

    Add project files.

---
+++ command/MainWindow.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+using command.Command;
+
+namespace command
+{
+    public partial class MainWindow : Window
+    {
+        public MessageCommand MessageCommand { get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext=this;
+
+            MessageCommand = new MessageCommand(Display);
+        }
+
+        public void Display()
+        {
+            MessageBox.Show("MessageCommandWorks");
+        }
+    }
+}
---
+++ command/Models/Car.cs
@@ -0,0 +1,9 @@
+﻿namespace command.Models
+{
+    public class Car : Entity
+    {
+        public string Vendor { get; set; }
+        public string Model { get; set; }
+        public string Year { get; set; }
+    }
+}
